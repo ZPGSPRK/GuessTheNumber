166f68c637a3414a2aadb973697a4ed1d75d1a4d
Zachary <>
2023-03-30 13:25:45 -0400

    Implemented input validation for introduction and number guessing.

--- src/main/java/GuessTheNumber.java
+++ src/main/java/GuessTheNumber.java
@@ -1,15 +1,29 @@
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class GuessTheNumber {
     public static void main(String[] args) {
         GuessTheNumber guessTheNumber = new GuessTheNumber();
-        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println(guessTheNumber.getIntroduction());
 
+        boolean isValidName;
+
+        do {
+            String name = scanner.nextLine();
+            isValidName = guessTheNumber.isValidName(name);
+            if (isValidName)
+                guessTheNumber.setName(name);
+            else
+                System.err.println("That name is invalid.");
+        } while (!isValidName);
+
+        System.out.println(String.format(guessTheNumber.getIntroductionResponse(), guessTheNumber.getName()));
+
             do
                 try {
-                    guessTheNumber.setGuess(Integer.parseInt(scan.nextLine()));
+                    guessTheNumber.setGuess(Integer.parseInt(scanner.nextLine()));
                 } catch (NumberFormatException e) {
                     System.err.println(guessTheNumber.getGuess() + " is not a valid number.");
                 }
@@ -18,16 +32,29 @@ public class GuessTheNumber {
         System.out.println(guessTheNumber.getExitPrompt());
     }
 
-
-
     private final String introduction;
 
+    public String getIntroductionResponse() {
+        return introductionResponse;
+    }
+
+    private final String introductionResponse;
     private final String exitPrompt;
     private Integer guess;
     private int secretNumber;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
     public GuessTheNumber() {
         this.introduction = "Hello! What is your name?";
+        this.introductionResponse = "Well, %s, I am thinking of a number between 1 and 20.\nTake a guess.";
         this.exitPrompt = "Would you like to play again? (y or n)";
         initializeSecretNumber();
         this.guess = null;
@@ -48,10 +75,17 @@ public class GuessTheNumber {
     }
 
     public boolean isWithinRange(Integer lowerBound, Integer upperBound) {
-        if (this.guess == null)
-            return false;
-        else
-            return guess >= lowerBound && guess <= upperBound;
+        boolean isWithinRange = guess >= lowerBound && guess <= upperBound;
+
+        if (!isWithinRange)
+            System.err.println("Number is not within range.");
+
+        return isWithinRange;
+    }
+
+    public boolean isValidName(String name) {
+        Pattern pattern = Pattern.compile("^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$");
+        return pattern.matcher(name).matches();
     }
 
     public Integer getGuess() {
@@ -60,10 +94,6 @@ public class GuessTheNumber {
 
     public void setGuess(Integer guess) {
         this.guess = guess;
-    }
-
-    public String generateResponse(String input) {
-        return "";
     }
 
     public String getIntroduction() {
